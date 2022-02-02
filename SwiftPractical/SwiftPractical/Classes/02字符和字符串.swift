5a402fc84d557057f6cb66ce5f30aa2951c5d6b4
ios_jason <ios_developjason@163.com>
2022-02-02 13:44:46 +0800

    基础语法

--- SwiftPractical/SwiftPractical/Classes/02字符和字符串.swift
+++ SwiftPractical/SwiftPractical/Classes/02字符和字符串.swift
@@ -112,4 +112,22 @@ class CharAndString: NSObject {
             print("NO")
         }
     }
+    
+    static func basicOperator() {
+        
+        /**
+         运算符是一元的、二元的或三元的
+         */
+        //一元前缀 !a ； 一元后缀 b!
+        
+        //二元中缀
+        let m = 10,n = 20
+        let k = m + n
+        
+        //三元
+        let a = false
+        a ? m : n
+        
+        
+    }
 }
